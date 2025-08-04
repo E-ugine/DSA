dc25fac8e285be959092d507a50e2c98285482ee
eugine agolla <eugineagolla@eugines-MacBook-Air.local>
2025-08-04 09:14:34 +0300

    Fresh look at python basics

---
+++ 101.py
@@ -0,0 +1,19 @@
+"""string = "I enjoy programming in Python."
+print("programming" in string)
+
+fruit = "Banana"
+for x in fruit:
+    print(x)
+
+    #Python slicing
+Greeting = "Good Morning"
+print(Greeting[3:5])
+
+text = "WE're the so called \"AI\" programmers"
+print(text)
+
+text = "eugine is dope"
+print(text.capitalize())
+"""
+
+
