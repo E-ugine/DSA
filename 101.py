5ecfdcfc1eca83a4431f8607ac11f7a4cefc872d
eugine agolla <eugineagolla@eugines-MacBook-Air.local>
2025-08-05 09:03:11 +0300

    python lists

--- 101.py
+++ 101.py
@@ -14,6 +14,27 @@ print(text)
 
 text = "eugine is dope"
 print(text.capitalize())
+
+myList = ["apple", "banana", "cherry"]
+myList.insert(2,"pie")
+print(myList)
+
+myList = ["apple", "banana", "cherry"]
+myList.append("pie")
+print(myList)
+
+myList = ["apple", "banana", "cherry"]
+hisList = ["orange", "kiwi", "melon"]
+myList.extend(hisList)
+print(myList)
+
+myList = ["apple", "banana", "cherry", "orange", "kiwi", "melon"]
+for i in range(len(myList)):
+    print(myList[i])
+
+#List Comprehension 
+Teams = ["Man Utd", "Chelsea", "Arsenal", "Liverpool", "Tottenham"]
+newTeams = [x for x in Teams if "l" in x]
+print(newTeams) 
+
 """
-
-
